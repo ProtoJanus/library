e476abfb12f79400f9fed4df41b7b511571be3be
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2025-01-21 16:17:53 -0600

    Add a dialog box that pops up when you want to add a book and basic styling for display purposes

--- scripts.js
+++ scripts.js
@@ -6,7 +6,29 @@ function Book(title, author, pages, hasRead) {
 }
 
 Book.prototype.info = function () {
-  return `${title} by ${author}, ${pages} pages, ${
-    hasRead ? "has read" : "has not read yet."
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.hasRead ? "has read" : "has not read yet."
   }`;
 };
+
+function addBookToLibrary(title, author, pages, hasRead) {
+  const book = new Book(title, author, pages, hasRead);
+  myLibrary.push(book);
+}
+
+function generateLibraryTable(book) {}
+
+const myLibrary = [];
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 300, true);
+
+const addBookButton = document.querySelector(".add-book-button");
+const dialog = document.querySelector("dialog");
+const modalClose = document.querySelector(".modal-close");
+
+addBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+modalClose.addEventListener("click", () => {
+  dialog.close();
+});
