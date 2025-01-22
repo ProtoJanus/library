c45428336127ee28c484de24a0c1811583f7f3f5
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2025-01-22 14:43:00 -0600

    Update the finished column to allow for toggling between Finished or Unfinished, and changes are consistent across front-end and in the psuedo back-end

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-function Book(title, author, pages, hasRead) {
+function Book(title, author, pages, hasRead, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.hasRead = hasRead;
+  this.id = id;
 }
 
 Book.prototype.info = function () {
@@ -11,24 +12,43 @@ Book.prototype.info = function () {
   }`;
 };
 
+Book.prototype.toggleHasRead = function () {
+  this.hasRead ? (this.hasRead = false) : (this.hasRead = true);
+  updateHasRead(this.id);
+};
+
 function addBookToLibrary(title, author, pages, hasRead) {
-  const book = new Book(title, author, pages, hasRead);
+  id += 1;
+  const book = new Book(title, author, pages, hasRead, id);
   myLibrary.push(book);
   generateLibraryTable(book);
 }
 
 function generateLibraryTable(book) {
   const booksTableBody = document.querySelector(".books-table-body");
-  booksTableBody.innerHTML += `<tr>
+  booksTableBody.innerHTML += `<tr id=${id}>
           <td>${book.title}</td>
           <td>${book.author}</td>
           <td>${book.pages}</td>
-          <td>${book.hasRead}</td>
+          <td class="has-read-cell">${
+            book.hasRead ? "True" : "False"
+          } <button class="has-read-toggle-button">Toggle</button></td>
         </tr>`;
 }
 
+function updateHasRead(rowId) {
+  const book = myLibrary.find((item) => item.id == rowId);
+  row = document.getElementById(rowId);
+  const hasReadCell = row.querySelector(".has-read-cell");
+
+  hasReadCell.firstChild.textContent = book.hasRead ? "True " : "False ";
+}
+
+let id = 0;
 const myLibrary = [];
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 300, true);
+addBookToLibrary("The Martian", "Andy Weir", 416, true);
+addBookToLibrary("Fool Moon", "Jim Butcher", 432, false);
 
 const addBookButton = document.querySelector(".add-book-button");
 const dialog = document.querySelector("dialog");
@@ -50,4 +70,16 @@ modalClose.addEventListener("click", (e) => {
   );
 
   dialog.close();
+});
+
+const hasReadToggleButtons = document.querySelectorAll(
+  ".has-read-toggle-button"
+);
+hasReadToggleButtons.forEach((hasReadToggleButton) => {
+  hasReadToggleButton.addEventListener("click", (e) => {
+    const row = e.target.closest("tr");
+    const specificBook = myLibrary.find((item) => item.id == row.id);
+
+    specificBook.toggleHasRead();
+  });
 });
